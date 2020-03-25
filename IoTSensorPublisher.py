e87cd8a071455bca289d118d0e8679d795b19ff7
RobSorce <sorce.1617825@studenti.uniroma1.it>
2020-03-25 23:21:58 +0100

    Update IoTSensorPublisher.py

--- IoTSensorPublisher.py
+++ IoTSensorPublisher.py
@@ -9,25 +9,39 @@ from azure.iot.device import IoTHubDeviceClient, MethodResponse
 if __name__ == "__main__":
     # Fetch the connection string from an enviornment variable
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
+    conn_str2 = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING2")
 
     # Create instance of the device client using the connection string
-    device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    device_client1 = IoTHubDeviceClient.create_from_connection_string(conn_str)
+    device_client2 = IoTHubDeviceClient.create_from_connection_string(conn_str2)
 
-    # Connect the device client.
-    device_client.connect()
+    # Connect the device clients.
+    device_client1.connect()
+    device_client2.connect()
     
     while(True):
-        envData = {'temperature': random.randrange(-50, 50),
+        envData1 = {'temperature': random.randrange(-50, 50),
                 'humidity': random.randrange(0, 100),
                 'windIntensity':random.randrange(0, 100),
                 'windDirection':random.randrange(0, 360),
                 'rainHeight':random.randrange(0, 50)
                 }
-        print((json.dumps(envData)))
+        envData2 = {'temperature': random.randrange(-50, 50),
+                'humidity': random.randrange(0, 100),
+                'windIntensity':random.randrange(0, 100),
+                'windDirection':random.randrange(0, 360),
+                'rainHeight':random.randrange(0, 50)
+                }
+        print((json.dumps(envData1)))
+        print((json.dumps(envData2)))
         # Send a single message
         print("Sending message...")
-        device_client.send_message(json.dumps(envData))
-        print("Message successfully sent!")
+        device_client1.send_message(json.dumps(envData1))
+        print("Message from device 1 successfully sent!")
+        time.sleep(2)
+        device_client2.send_message(json.dumps(envData2))
+        print("Message from device 2 successfully sent!")
         time.sleep(2)
     # finally, disconnect
-    device_client.disconnect()
+    device_client1.disconnect()
+    device_client2.disconnect()
