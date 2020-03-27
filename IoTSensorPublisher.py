a0a79bc335aa388d9011f0560bc791f8d1b6bdcd
RobSorce <sorce.1617825@studenti.uniroma1.it>
2020-03-27 22:28:09 +0100

    Minor changes, added more comments

--- IoTSensorPublisher.py
+++ IoTSensorPublisher.py
@@ -6,6 +6,13 @@ import json
 import os
 from azure.iot.device import IoTHubDeviceClient, MethodResponse
 
+"""
+# This code generates random values for 5 different environmental sensors, 
+# grouped into a virtual device on the Azure IoT Hub:  
+# The data will be published on a MQTT channel (by default using Python),
+# then the values can be read by the web app in order to plot the values on the charts.
+"""
+
 if __name__ == "__main__":
     # Fetch the connection string from an enviornment variable
     conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
@@ -18,7 +25,8 @@ if __name__ == "__main__":
     # Connect the device clients.
     device_client1.connect()
     device_client2.connect()
-    
+
+    # Generate the random data for the devices (between the given range)
     while(True):
         envData1 = {'temperature': random.randrange(-50, 50),
                 'humidity': random.randrange(0, 100),
